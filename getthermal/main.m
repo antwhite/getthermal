268ac7a3d16944ca29679bfde641d2700236adcf
Douglas Floyd <dfloyd888@me.com>
2015-12-06 21:34:47 -0600

    Initial Commit

---
+++ getthermal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  getthermal
+//
+//  Created by Douglas Floyd on 12/6/15.
+//  Copyright © 2015 Secure Bear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
