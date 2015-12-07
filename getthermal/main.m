bb22208578f6e3ca2bf522ab4d4c2a477f5e1b6d
Douglas Floyd <dfloyd888@me.com>
2015-12-06 21:57:16 -0600

    Original file, with return codes and output

--- getthermal/main.m
+++ getthermal/main.m
@@ -5,13 +5,33 @@
 //  Created by Douglas Floyd on 12/6/15.
 //  Copyright © 2015 Secure Bear. All rights reserved.
 //
+// Grabs the thermal pressure state, returns it as an error code.
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSProcessInfoThermalState state = [[NSProcessInfo processInfo] thermalState];
+        
+        if (state == NSProcessInfoThermalStateFair) {
+            printf ("1\n");
+            return 1;
+            // Thermals are fair. Consider taking proactive measures to prevent higher thermals.
+        } else if (state == NSProcessInfoThermalStateSerious) {
+            printf ("2\n");
+            return 2;
+            // Thermals are highly elevated. Help the system by taking corrective action.
+        } else if (state == NSProcessInfoThermalStateCritical) {
+            printf ("3\n");
+            return 3;
+            // Thermals are seriously elevated. Help the system by taking immediate corrective action.
+        } else {
+            printf ("0\n");
+            return 0;
+            // Thermals are okay. Go about your business.
+        };
+        
     }
-    return 0;
+    
 }
